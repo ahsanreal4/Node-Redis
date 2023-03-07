c3096fec192e2635230c51b3c77683302f6bc3dc
ahsanreal4 <ahsan.btph123@gmail.com>
2023-03-07 13:45:12 +0500

    redis connection done

---
+++ config/redis.js
@@ -0,0 +1,11 @@
+const redis = require('redis');
+
+const client = redis.createClient();
+client.connect();
+
+client.on('error', (err) => console.log('Redis Client Error', err));
+client.on('connect', () =>
+  console.log('=========> Redis Connected <==========')
+);
+
+module.exports = client;
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const client = require('./config/redis');
 const cors = require('cors');
 
 const PORT = 8000;
 
 const app = express();
 app.use(cors());
+
+app.use('/api', require('./routes/api'));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
