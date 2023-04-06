758ff7c46cec23bcba58341e2fc1629430285cb0
ahsan.btph123@gmail.com <ahsan.btph123@gmail.com>
2023-04-06 13:25:01 +0500

    getting json data for redis functions

--- common/functions/redis.functions.js
+++ common/functions/redis.functions.js
@@ -1,17 +1,25 @@
 const client = require("../config/redis.config");
 
-async function getRedisValue(key) {
+async function getRedisValue(key, type = "string") {
   if (!key || key.length === 0) {
     console.log("Key was not provided to getRedisValue() function");
     return;
   }
-
+  if (type.toLocaleLowerCase() === "json") {
+    const jsonKey = await client.get(key);
+    return JSON.parse(jsonKey);
+  }
   return await client.get(key);
 }
 
-function setRedisKey(key, value) {
+function setRedisKey(key, value, type = "string") {
   if (!key || key.length === 0 || !value || value.length === 0) {
     console.log("Key or value was not provided to setRedisKey() function");
+    return;
+  }
+
+  if (type.toLocaleLowerCase() === "json") {
+    client.set(key, JSON.stringify(value));
     return;
   }
 
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const { getRedisValue } = require("../common/functions/redis.functions");
+const {
+  getRedisValue,
+  setRedisKey,
+} = require("../common/functions/redis.functions");
 
-router.get("/", async (req, res) => {
-  const value = await getRedisValue("hello");
+router.get("/get", async (req, res) => {
+  const value = await getRedisValue("hello", "json");
   if (value) {
     return res.send(value);
   }
   res.send("Value not found");
+});
+
+router.get("/set", async (req, res) => {
+  setRedisKey("hello", { message: "storing object" }, "json");
+  res.send("Successful");
 });
 
 module.exports = router;
